b000518721516e95c5e376de858ce20bb0bc90b1
Jiwon <jae2021@gmail.com>
2026-04-28 12:34:13 +0900

    빌드 업데이트
    빌드 업데이트

--- DD_Sample.js
+++ DD_Sample.js
@@ -71,7 +71,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\popo2\AppData\Local\Temp\tmpx_2cubic.js
+// include: C:\Users\popo2\AppData\Local\Temp\tmp71dp25ca.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -204,21 +204,21 @@ Module['FS_createPath']("/", "Asset", true, true);
 
   })();
 
-// end include: C:\Users\popo2\AppData\Local\Temp\tmpx_2cubic.js
-// include: C:\Users\popo2\AppData\Local\Temp\tmp93ucj_if.js
+// end include: C:\Users\popo2\AppData\Local\Temp\tmp71dp25ca.js
+// include: C:\Users\popo2\AppData\Local\Temp\tmpv8p84jmo.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\popo2\AppData\Local\Temp\tmp93ucj_if.js
-// include: C:\Users\popo2\AppData\Local\Temp\tmpcgk9t4w_.js
+  // end include: C:\Users\popo2\AppData\Local\Temp\tmpv8p84jmo.js
+// include: C:\Users\popo2\AppData\Local\Temp\tmpf3s2m6fm.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\popo2\AppData\Local\Temp\tmpcgk9t4w_.js
+  // end include: C:\Users\popo2\AppData\Local\Temp\tmpf3s2m6fm.js
 
 
 var arguments_ = [];
